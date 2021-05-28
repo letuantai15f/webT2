9f2370fbf90fd188e1af6081f7d3c2c5e707e244
letuantai15f <letuantai15f@gmail.com>
2021-05-28 20:53:32 +0700

    up

--- jquery.js
+++ jquery.js
@@ -1,50 +1,57 @@
 // JavaScript Document
+
 var inputUsername=document.getElementById("username");
 var inputPass=document.getElementById("pass");
 var btndangnhap=document.getElementById("btndangnhap");
 btndangnhap.onclick = function checkdangnhap()
 
 {
+	
 	var username=inputUsername.value;
 	var pass=inputPass.value;
-
-	if(username=='admin' && pass=='admin')
-		alert('dang nhap thanh cong');
-	else
-		alert('khong thanh cong');
-
-}
-
-var row=1;
-var iphoten=document.getElementById("hoten");
-	var ipsdt=document.getElementById("sdt");
-	var ipemail=document.getElementById("email");
-	var ipuser=document.getElementById("user");
-	var ippassw=document.getElementById("passw");
-var dangki =document.getElementById("dangki");
-dangki.onclick=function displaydetail()
-{
-	var hoten=iphoten.value;
-	var sdt=ipsdt.value;
-	var email=ipemail.value;
-	var user=ipuser.value;
-	var passw=ippassw.value;
-	if(!hoten||!sdt || !email || !user || ! passw){
-			alert('Vui lòng nhập đủ thông tin');
-	return;
-	}
-	var display=document.getElementById("display");
-	var newrow=display.insertRow(row);
-	var cell1=newrow.insertCell(0);
-	var cell2=newrow.insertCell(1);
-	var cell3=newrow.insertCell(2);
-	var cell4=newrow.insertCell(3);
-	var cell5=newrow.insertCell(4);
-	cell1.innerHTML=hoten;
-	cell2.innerHTML=sdt;
-	cell3.innerHTML=email;
-	cell4.innerHTML=user;
-	cell5.innerHTML=passw;
-	row++;
 	
+
+	var x=JSON.parse(localStorage.getItem('mydata')) || [];
+	for(var i=0;i<x.length;i++){
+		var t=x[i].user;
+		var y=x[i].passw;
+		if(username===x[i].user && pass===x[i].passw)
+			alert('thanh cong');
+		else
+			alert('that bai');
+		
+	}
+
 }
+
+
+
+
+let datas=[];
+const adddata=(ev)=>{
+	ev.preventDefault();
+	let data={
+	hoten:document.getElementById("hoten").value,
+	 sdt:document.getElementById("sdt").value,
+	 email:document.getElementById("email").value,
+	 user:document.getElementById("user").value,
+	 passw:document.getElementById("passw").value
+
+	}
+	datas.push(data);
+	console.warn('added',{datas});
+	localStorage.setItem('mydata',JSON.stringify(datas));
+}
+document.addEventListener('DOMContentLoaded',()=>{
+	document.getElementById('dangki').addEventListener('click',adddata);
+});
+
+
+
+
+
+
+
+
+
+
