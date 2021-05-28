d4cb5e7fbba1579b9ba73a15c4fb7e368f48da12
letuantai15f <letuantai15f@gmail.com>
2021-05-28 09:31:12 +0700

    up

--- jquery.js
+++ jquery.js
@@ -14,3 +14,37 @@ btndangnhap.onclick = function checkdangnhap()
 		alert('khong thanh cong');
 
 }
+
+var row=1;
+var iphoten=document.getElementById("hoten");
+	var ipsdt=document.getElementById("sdt");
+	var ipemail=document.getElementById("email");
+	var ipuser=document.getElementById("user");
+	var ippassw=document.getElementById("passw");
+var dangki =document.getElementById("dangki");
+dangki.onclick=function displaydetail()
+{
+	var hoten=iphoten.value;
+	var sdt=ipsdt.value;
+	var email=ipemail.value;
+	var user=ipuser.value;
+	var passw=ippassw.value;
+	if(!hoten||!sdt || !email || !user || ! passw){
+			alert('Vui lòng nhập đủ thông tin');
+	return;
+	}
+	var display=document.getElementById("display");
+	var newrow=display.insertRow(row);
+	var cell1=newrow.insertCell(0);
+	var cell2=newrow.insertCell(1);
+	var cell3=newrow.insertCell(2);
+	var cell4=newrow.insertCell(3);
+	var cell5=newrow.insertCell(4);
+	cell1.innerHTML=hoten;
+	cell2.innerHTML=sdt;
+	cell3.innerHTML=email;
+	cell4.innerHTML=user;
+	cell5.innerHTML=passw;
+	row++;
+	
+}
