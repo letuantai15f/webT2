48e158964c556eb364acf19ea28f4bd585d575db
letuantai15f <letuantai15f@gmail.com>
2021-05-29 16:58:26 +0700

    tai

--- jquery.js
+++ jquery.js
@@ -4,6 +4,7 @@ var inputUsername=document.getElementById("username");
 var inputPass=document.getElementById("pass");
 var btndangnhap=document.getElementById("btndangnhap");
 
+
 btndangnhap.onclick = function checkdangnhap()
 
 {
@@ -31,6 +32,8 @@ btndangnhap.onclick = function checkdangnhap()
 		document.getElementById('btndangxuat').style.display='block';
 		document.getElementById('username').style.display='none';
 		document.getElementById('pass').style.display='none';
+			
+		
 		var btndangxuat=document.getElementById('btndangxuat');
 			btndangxuat.onclick=function xoa(){
 				inputUsername.value="";
@@ -54,23 +57,115 @@ btndangnhap.onclick = function checkdangnhap()
 
 
 let datas=[];
-const adddata=(ev)=>{
-	ev.preventDefault();
+//const adddata=(ev)=>{
+//	ev.preventDefault();
+//	let data={
+//		
+//	 hoten:document.getElementById("hoten").value,
+//	 sdt:document.getElementById("sdt").value,
+//	 email:document.getElementById("email").value,
+//	 user:document.getElementById("user").value,
+//	 passw:document.getElementById("passw").value
+//
+//	}
+//	
+//	datas.push(data);
+//	console.warn('added',{datas});
+//	localStorage.setItem('mydata',JSON.stringify(datas));
+//	
+//}
+function adddata(){
 	let data={
-	hoten:document.getElementById("hoten").value,
+		
+	 hoten:document.getElementById("hoten").value,
 	 sdt:document.getElementById("sdt").value,
 	 email:document.getElementById("email").value,
 	 user:document.getElementById("user").value,
 	 passw:document.getElementById("passw").value
 
 	}
+	
 	datas.push(data);
 	console.warn('added',{datas});
 	localStorage.setItem('mydata',JSON.stringify(datas));
 }
+
+
+ 
 document.addEventListener('DOMContentLoaded',()=>{
-	document.getElementById('dangki').addEventListener('click',adddata);
+document.getElementById("dangki").addEventListener('click',function(){
+	if(checkPhone() && validateemail() && validateform()){
+		adddata();
+		var data=JSON.parse(localStorage.getItem('mydata')) || [];
+		alert('Đăng kí thành công \n tên đăng nhập của bạn:     '+ document.getElementById("user").value + '\n Số Điện Thoại:    ' + document.getElementById("sdt").value + '\n Email:    ' + document.getElementById("email").value  + '\n XIN CHÀO  ' + document.getElementById("hoten").value );
+		location.reload();
+	}
+	
 });
+	
+	
+});
+
+	
+	
+
+//-------------------------------------------
+
+function validateform(){  
+var name=document.getElementById("user").value;  
+var password=document.getElementById("passw").value;  
+  
+if (name=="" || password==""){  
+  alert("Không Được Để Trống");  
+  return false;  
+}else if(password.length<6){  
+  alert("Mật Khẩu Phải  6 Kí Tự Trở Lên");  
+  return false;  
+  }  
+	else return true;
+} 
+
+function matchpass(){  
+var firstpassword=document.getElementById("passw").value;  
+var secondpassword=document.getElementById("passw2").value;  
+  
+if(firstpassword==secondpassword){  
+return true;  
+}  
+else{  
+alert("password Phải Giống Nhau!");  
+return false;  
+}  
+	return true;
+}  
+
+
+function validateemail()  
+{  
+var x=document.getElementById("email").value;  
+var atposition=x.indexOf("@");  
+var dotposition=x.lastIndexOf(".");  
+if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+  alert('Lỗi Định Dạng Mail');  
+  return false;  
+  }
+	else return true;
+}  
+
+
+ function checkPhone() {
+        var phone = document.getElementById("sdt").value;
+        var phoneNum = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/; 
+            if(phone.match(phoneNum)) {
+                return true;
+            }
+            else {
+                alert('vui long không nhập ký tự - SĐT:0xxxxxxxxxx')
+            }
+        }
+
+
+
 
 
 
